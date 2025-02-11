6cac4a0c9581de51fdc00a92db1166ce17214476
Lukas <lukas.ledauskis@stud.smk.lt>
2025-02-11 19:47:28 +0200

    LangQuest pirma versija

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Image, SafeAreaView, StatusBar, Text, View } from "react-native";
+import question from '../assets/data/oneQuestionWithOption';
+import ImageOption from "~/components/ImageOption";
+
+export default function Home() {
+  return (
+    <SafeAreaView className="flex flex-1 items-center justify-center p-3">
+      <StatusBar animated barStyle={'light-content'}/>
+      <Text className="mb-4 text-center text-2xl font-bold">{question.question}</Text>
+
+      <View className="w-full flex-1 flex-row flex-wrap justify-between">
+        {question.options.map((option) => (
+          <ImageOption key={option.id} image={option.image} text={option.text}/>
+        ))}
+      </View>
+    </SafeAreaView> 
+  );
+}
